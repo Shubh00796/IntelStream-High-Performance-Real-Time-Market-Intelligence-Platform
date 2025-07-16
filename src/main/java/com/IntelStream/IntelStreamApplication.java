24f620c1939b99e4bfe259f62c7103e0f431f84f
Shubham Dongare <shubhamdongare9696@gmail.com>
2025-07-16 13:07:39 +0530

    feat(IntelStream): Initital commit

---
+++ src/main/java/com/IntelStream/IntelStreamApplication.java
@@ -0,0 +1,13 @@
+package com.IntelStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IntelStream/IntelStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IntelStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
