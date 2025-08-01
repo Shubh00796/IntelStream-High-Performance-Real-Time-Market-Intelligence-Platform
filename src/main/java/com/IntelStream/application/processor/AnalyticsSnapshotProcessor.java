01ce15c4cef80c3876c9cc763be8309c5f749635
Shubh00796 <shubhamdongare9696@gmail.com>
2025-08-01 12:26:40 +0530

    (feat-intelStream): added the counting, mapping and grouping by queries for for analytics snapshots implemented the filter query features

--- src/main/java/com/IntelStream/application/processor/AnalyticsSnapshotProcessor.java
+++ src/main/java/com/IntelStream/application/processor/AnalyticsSnapshotProcessor.java
@@ -19,8 +19,56 @@ public class AnalyticsSnapshotProcessor {
     public List<AnalyticsSnapshot> filterOverBroughtSnapshots() {
         return snapshots
                 .stream()
-                .filter(analyticsSnapshot -> analyticsSnapshot.isOverbought())
-                .collect(Collectors.toList());
+                .filter(AnalyticsSnapshot::isOverbought)
+                .toList();
+    }
+
+    //2. Filter Oversold Snapshots
+    public List<AnalyticsSnapshot> filterOversoldSnapshots() {
+        return snapshots
+                .stream()
+                .filter(AnalyticsSnapshot::isOversold)
+                .toList();
+    }
+
+    //3. Filter by Volatility Above Threshold
+    public List<AnalyticsSnapshot> filterByVolatilityAboveThreshold(BigDecimal threshold) {
+        return snapshots
+                .stream()
+                .filter(analyticsSnapshot -> analyticsSnapshot.getVolatility() != null && analyticsSnapshot.getVolatility().compareTo(threshold) > 0)
+                .toList();
+    }
+
+    //4. Filter By RSI in between
+    public List<AnalyticsSnapshot> filterRSIInBetween(BigDecimal max, BigDecimal Min) {
+        return snapshots
+                .stream()
+                .filter(analyticsSnapshot -> analyticsSnapshot.getRsi() != null && analyticsSnapshot.getRsi().compareTo(max) < 0 && analyticsSnapshot.getRsi().compareTo(Min) > 0)
+                .toList();
+    }
+
+    //5. Filter by Price change percent above
+    public List<AnalyticsSnapshot> filterByPriceChangePercentAbove(BigDecimal threshold) {
+        return snapshots
+                .stream()
+                .filter(analyticsSnapshot -> analyticsSnapshot.getPriceChangePercent24h().compareTo(threshold) > 0)
+                .toList();
+    }
+
+    //7. Filter by Volume above
+    public List<AnalyticsSnapshot> filterByVolumeAbove(BigDecimal threshold) {
+        return snapshots
+                .stream()
+                .filter(analyticsSnapshot -> analyticsSnapshot.getVolume24h() != null && analyticsSnapshot.getVolume24h().compareTo(threshold) > 0)
+                .toList();
+    }
+
+    //8. Filter by Instrument Id and RsI
+    public List<AnalyticsSnapshot> filterByInstrumentIdAndRsi(Long instrumentId, BigDecimal rsi) {
+        return snapshots
+                .stream()
+                .filter(analyticsSnapshot -> analyticsSnapshot.getInstrumentId().equals(instrumentId) && analyticsSnapshot.getRsi().equals(rsi))
+                .toList();
     }
 
 
